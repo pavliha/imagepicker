629b100ba2025efe6879c7c1fc8cac619dcaa1f6
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2016-05-30 23:00:20 +0300

    refactor blade files

---
+++ resources/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+  <meta name="description"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta name="msapplication-tap-highlight" content="no"/>
+  <meta name="mobile-web-app-capable" content="yes"/>
+  <meta name="application-name" content="Image Filters Online - быстрая обработка фото бесплатно"/>
+  <meta name="theme-color" content="#009db5"/>
+  <link rel="icon" sizes="192x192" href="images/touch/chrome-touch-icon-192x192.png"/>
+  <meta charset="UTF-8">
+  <title>Image Filters Online - быстрая обработка фото бесплатно</title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+  <link rel="stylesheet" href="/styles/main.css"/>
+</head>
+<body>
+@yield("body")
+</body>
+</html>
---
+++ resources/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends("layouts.main")
+
+@section('head', '<title>Image Picker</title>')
+
+@section("body")
+    <section id="app"></section>
+    <footer></footer>
+    <script src="/bower_components/jquery/dist/jqueryAjax.js"></script>
+    <script async src="/bower_components/jq-ajax-progress/src/jq-ajax-progress.min.js"></script>
+    <script async src="/bower_components/caman/dist/caman.min.js"></script>
+    <script src="/js/main.js"></script>
+@endsection
