3de7987d37d50c80d98da64b41bd805e311231d9
Pavel Kostyuk <kostyk.pavel.09@gmail.com>
2016-06-09 01:19:29 +0300

    add manifest.json

--- resources/layouts/main.blade.php
+++ resources/layouts/main.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+  <link rel="manifest" href="manifest.json">
   <meta charset="utf-8"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
   <meta name="description"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <meta name="msapplication-tap-highlight" content="no"/>
   <meta name="mobile-web-app-capable" content="yes"/>
-  <meta name="application-name" content="Image Filters Online - быстрая обработка фото бесплатно"/>
+  <meta name="application-name" content="Image Filters"/>
+
   <meta name="theme-color" content="#009db5"/>
   <meta charset="UTF-8">
   <link rel="icon" sizes="192x192" href="images/touch/chrome-touch-icon-192x192.png"/>
